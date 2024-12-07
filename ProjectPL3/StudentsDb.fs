f959dc8bcebebedc471201092695b51ddb98ba57
unknown <nouranyasserhussein@gmail.com>
2024-12-06 21:42:28 -0800

    AddUpdateDeleteFunctions

--- ProjectPL3/StudentsDb.fs
+++ ProjectPL3/StudentsDb.fs
@@ -16,3 +16,12 @@ let loadStudents() =
 // Save students to a JSON file
 let saveStudents (students: Student list) =
     File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(students, Formatting.Indented))
+
+// Add, edit, and remove students
+let addStudent (student: Student) (db: Student list) = student :: db
+
+let editStudent (id: int) (newStudent: Student) (db: Student list) =
+    db |> List.map (fun s -> if s.ID = id then newStudent else s)
+
+let removeStudent (id: int) (db: Student list) =
+    db |> List.filter (fun s -> s.ID <> id)
